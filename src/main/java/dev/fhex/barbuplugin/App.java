0741e7eeaf830fdba6ba3a1f53d406fc38e32b0b
Sylvain VISTE <viste.sylvain@gmail.com>
2020-03-07 18:04:33 +0100

    Ajout de l'affichage de la santé sur la liste des joueurs

--- src/main/java/dev/fhex/barbuplugin/App.java
+++ src/main/java/dev/fhex/barbuplugin/App.java
@@ -3,19 +3,24 @@ package dev.fhex.barbuplugin;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Statistic;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.RenderType;
 import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 
 public class App extends JavaPlugin implements Listener {
     private Scoreboard scoreboard;
     private Objective timeSinceRestObjective;
     private Objective deathCountObjective;
+    private Objective listObjective;
     private boolean state = false;
 
     @Override
@@ -33,8 +38,14 @@ public class App extends JavaPlugin implements Listener {
                 ChatColor.RED + getConfig().getString("time_since_rest_label"));
         deathCountObjective = scoreboard.registerNewObjective("death", "dummy",
                 ChatColor.RED + getConfig().getString("death_label"));
+        listObjective = scoreboard.registerNewObjective("player_list", "dummy", "Liste des joueurs");
+
+        listObjective.setRenderType(RenderType.HEARTS);
+        listObjective.setDisplaySlot(DisplaySlot.PLAYER_LIST);
 
         timeSinceRestObjective.setDisplaySlot(DisplaySlot.SIDEBAR);
+
+        listObjective.getScore("Garbage").setScore(0);
 
         // Set our scoreboard to all players on the server
         for (Player p : Bukkit.getOnlinePlayers()) {
@@ -49,6 +60,9 @@ public class App extends JavaPlugin implements Listener {
                     timeSinceRestObjective.getScore(p.getDisplayName()).setScore(value / 24000);
                     int value2 = p.getStatistic(Statistic.DEATHS);
                     deathCountObjective.getScore(p.getDisplayName()).setScore(value2);
+
+                    double health = p.getHealth();
+                    listObjective.getScore(p.getDisplayName()).setScore((int) health);
                 }
             }
         }, 0, 20);
@@ -70,5 +84,15 @@ public class App extends JavaPlugin implements Listener {
     public void onPlayerJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         p.setScoreboard(scoreboard);
+    }
+
+    @EventHandler
+    public void onServerListPing(ServerListPingEvent e) {
+        World w = Bukkit.getWorlds().get(0);
+        long ticks = w.getTime();
+        long minute = ticks / 1200;
+        long second = ticks / 20 - minute * 60;
+        String s = "" + Math.round(minute) + ":" + Math.round(second);
+        e.setMotd(ChatColor.BOLD + "Barbus Land" + ChatColor.RESET + " - " + s);
     }
 }
